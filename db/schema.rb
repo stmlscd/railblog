b06b2ee2496af8e432674afc05dcff07a8ec6175
Sang Cheol Lee <sellma0080@gmail.com>
2020-06-19 23:28:29 +0900

    Install bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2020_06_17_113204) do
 
+  create_table "arricles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
